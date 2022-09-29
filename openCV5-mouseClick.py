c6b75619b12f519c11f8790d20ad17596284c0c7
Joe Sauceda <mecajoe@yahoo.com>
2022-09-28 23:51:10 -0400

    updated opencv5

--- openCV5-mouseClick.py
+++ openCV5-mouseClick.py
@@ -1,10 +1,15 @@
 from ctypes.wintypes import PINT
 import cv2
+import numpy as np
 
 print(cv2.__version__)
 
 # Predefined mouse event
 evt=-1
+# Empty array to store coordinates, global in python
+coord=[]
+
+img=np.zeros((250,250,3),np.uint8)
 
 # Click function for Mouse Callback
 def click(event,x,y,flag,params):
@@ -15,13 +20,30 @@ def click(event,x,y,flag,params):
         print('mouse event was: ',event)
         print(x,',',y)
         pnt=(x,y)
+        coord.append(pnt)
         evt=event
+    # Retrieve BGR of right button press, open in new window
+    if event==cv2.EVENT_RBUTTONDOWN:
+        print(x,y)
+        blue=frame[y,x,0]
+        green=frame[y,x,1]
+        red=frame[y,x,2]
+        print(blue,green,red)
+        colorString=str(blue)+','+str(green)+','+str(red)
+        img[:]=[blue,green,red]
+        fnt=cv2.FONT_HERSHEY_PLAIN
+        r=255-int(red)
+        g=255-int(green)
+        b=255-int(blue) 
+        tp=(b,g,r)
+        cv2.putText(img,colorString,(10,25),fnt,1,tp,2)
+        cv2.imshow('myColor',img)
 
 dispW=640
 dispH=480
 flip=2
 
-# Mouse Action Callback
+# Listener for Mouse Action Callback
 cv2.namedWindow('nanoCam')
 cv2.setMouseCallback('nanoCam',click)
 
@@ -35,16 +57,19 @@ cam= cv2.VideoCapture(camSet)
 
 while True:
     ret, frame = cam.read()
-   
-    if evt==1:
-        # Print circle to screen upon mouse click
-        cv2.circle(frame,pnt,2,(0,0,255),-1)
-        # Print x,y text from circle
+    # Print coordinates and x,y text
+    for pnts in coord:
+        cv2.circle(frame,pnts,5,(0,0,255),-1)
         font=cv2.FONT_HERSHEY_PLAIN
-        myStr=str(pnt)
-        cv2.putText(frame,myStr,pnt,font,1,(255,0,0),2)
+        myStr=str(pnts)
+        cv2.putText(frame,myStr,pnts,font,1.5,(255,0,0),2)
+
     cv2.imshow('nanoCam',frame)
-    if cv2.waitKey(1)==ord('q'):
+    # keyEvent to quit or clear points
+    keyEvent=cv2.waitKey(1)
+    if keyEvent==ord('q'):
         break
+    if keyEvent==ord('c'):
+        coord=[]
 cam.release()
 cv2.destroyAllWindows()
