7df4a1697959c76ca4e31cc660c4e05da7fd23e8
jdsauceda <54692984+jdsauceda@users.noreply.github.com>
2022-10-28 23:58:35 -0400

    Update sdptest.py

--- sdptest.py
+++ sdptest.py
@@ -3,14 +3,17 @@ import jetson_utils
 import time
 import cv2
 import numpy as np
-import RPi.GPIO as GPIO  #GPIO Library
+import RPi.GPIO as GPIO
 import time
 import serial  
 
-width=640
-height=480
+width=800
+height=600
 
-# Configure UART
+# GPIO board configuration
+GPIO.setmode(GPIO.BOARD)
+
+# Configure UART communication 
 serial_port = serial.Serial(
     port="/dev/ttyTHS1",
     baudrate=115200,
@@ -18,14 +21,19 @@ serial_port = serial.Serial(
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
 )
-GPIO.setmode(GPIO.BOARD)
+
 # Wait for port to initialize
 time.sleep(1)
 
 # Instantiate Camera object
 cam=jetson_utils.gstCamera(width,height,'0')
-# Network to use for object detection
-net=jetson_inference.imageNet('resnet-18')
+
+# Standard image detection network
+net=jetson_inference.imageNet('googlenet') 
+
+# Transfer learning image detection network (nothing, pumpkin, raspberry pico)
+# net=jetson_inference.imageNet('googlenet', ['--model=/home/nano/Downloads/jetson-inference/python/training/classification/myModel/resnet18.onnx', '--input_blob=input_0','--output_blob=output_0','--labels=/home/nano/Downloads/jetson-inference/myTrain/labels.txt'])
+
 # Use time library for fps calculation
 timeMark=time.time()
 # Filter to smooth images
@@ -33,18 +41,23 @@ fpsFilter=0
 # OpenCV font
 font=cv2.FONT_HERSHEY_SIMPLEX
 
-# Loop to keep display open until closed
+# Loop
 while True:
-    # Capture frame
+    # Capture frame, specify width and height
     frame, width, height=cam.CaptureRGBA(zeroCopy=1)
+
     # Network Classifier to return ID and confidence level
     classID, confident=net.Classify(frame, width, height)
     # Changes classID number into item name
     item=net.GetClassDesc(classID)
 
-    # Write item to pico
+
+    # Write item to pico over UART
     serial_port.write(item.encode())
     print(item)
+
+    # Specify motor control aspect
+
 
     # Calculate time difference for fps
     dt=time.time()-timeMark
@@ -59,15 +72,18 @@ while True:
     frame=cv2.cvtColor(frame,cv2.COLOR_RGBA2BGR).astype(np.uint8)
     # Format text for openCV
     cv2.putText(frame,str(round(fpsFilter,1))+' fps '+ item,(0,30),font,1,(0,0,255),2)
+
+
     # Display image as recCam
-    cv2.imshow('recCam',frame)
+    cv2.imshow('Camera',frame)
     # Move display to 0,0 area
-    cv2.moveWindow('recCam',0,0)
+    cv2.moveWindow('Camera',0,0)
 
     # Loop to exit from recCam
     if cv2.waitKey(1)==ord('q'):
-        
         break
+
+        
 # Release and destroy windows
 cam.release()
 cv2.destroyAllWindows()
