20739c75c731ec03b60554e04f3323b0f50f301f
jdsauceda <54692984+jdsauceda@users.noreply.github.com>
2022-11-20 20:24:31 -0500

    using wide and slice aoi

--- sdptest3.py
+++ sdptest3.py
@@ -10,6 +10,9 @@ import serial
 dispW=800
 dispH=600
 flip=0
+preview = True
+wide_aoi = False
+slice_aoi = False
 
 # GPIO board configuration
 GPIO.setmode(GPIO.BOARD)
@@ -40,14 +43,6 @@ net = jetson_inference.imageNet(
     ]
 )
 
-while True:
-    # Capture frame, specify width and height
-    frame, dispW, dispH = cam.CaptureRGBA(zeroCopy=1)
-    # Convert cuda to numpy for openCV frame format
-    frame = jetson_utils.cudaToNumpy(frame,dispW,dispH,4)
-    frame = cv2.cvtColor(frame,cv2.COLOR_RGBA2BGR).astype(np.uint8)
-    cv2.imshow('Preview',frame)
-
 # Use time library for fps calculation
 timeMark = time.time()
 # Filter to smooth imagesq
@@ -57,65 +52,128 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 
 # Loop to display captured frames
 while True:
-    # Capture frame, specify width and height
-    frame, dispW, dispH = cam.CaptureRGBA(zeroCopy=1)
-    roi = frame
-    # # Network Classifier to return ID and confidence level
-    # classID, confident = net.Classify(frame, width, height)
-    # # Changes classID number into item name
-    # item = net.GetClassDesc(classID)
-
-
-    # Write item to pico over UART
-    # try:
-    #     if item != 'mail':
-    # serial_port.write(item.encode())
-
-    # except:
-    #     print('Continuing')
-
-    # Motor control ?
     
+    if preview:
+        frame, dispW, dispH = cam.CaptureRGBA(zeroCopy=1)
+        frame = jetson_utils.cudaToNumpy(frame,dispW,dispH,4)
+        frame = cv2.cvtColor(frame,cv2.COLOR_RGBA2BGR).astype(np.uint8)
+        cv2.imshow('Preview',frame)
+        if cv2.waitKey(1) == ord('a'):
+            preview = False
+            wide_aoi = True
+            slice_aoi = False
+            cam.Close()
+            cv2.destroyAllWindows()
+        if cv2.waitKey(1) == ord('r'):
+            preview = False
+            wide_aoi = False
+            slice_aoi =True
+            cam.Close()
+            cv2.destroyAllWindows()
 
-    # Calculate time difference for fps
-    dt = time.time()-timeMark
-    # fps calculation
-    fps = 1/dt
-    # Smoothing calculation using Filter
-    fpsFilter = .95*fpsFilter + .05*fps
-    # timeMark for Filter
-    timeMark = time.time()
-    # Convert cuda to numpy for openCV frame format
-    frame = jetson_utils.cudaToNumpy(frame,dispW,dispH,4)
-    frame = cv2.cvtColor(frame,cv2.COLOR_RGBA2BGR).astype(np.uint8)
+    if slice_aoi:
+        frame, dispW, dispH = cam.CaptureRGBA(zeroCopy=1)
+        roi = frame
+        # Calculate time difference for fps
+        dt = time.time()-timeMark
+        # fps calculation
+        fps = 1/dt
+        # Smoothing calculation using Filter
+        fpsFilter = .95*fpsFilter + .05*fps
+        # timeMark for Filter
+        timeMark = time.time()
+        # Convert cuda to numpy for openCV frame format
+        frame = jetson_utils.cudaToNumpy(frame,dispW,dispH,4)
+        frame = cv2.cvtColor(frame,cv2.COLOR_RGBA2BGR).astype(np.uint8)
 
+        # Region of Interest   
+        # Network Classifier to return ID and confidence level
+        classID, confident = net.Classify(roi, dispW, dispH)
+        # Changes classID number into item name
+        item = net.GetClassDesc(classID)
 
-    # Region of Interest   
-    # Network Classifier to return ID and confidence level
-    classID, confident = net.Classify(roi, dispW, dispH)
-    # Changes classID number into item name
-    item = net.GetClassDesc(classID)
+        # Convert cuda to numpy for openCV frame format
+        roi = jetson_utils.cudaToNumpy(roi,dispW,dispH,4)
+        roi = cv2.cvtColor(roi,cv2.COLOR_RGBA2BGR).astype(np.uint8)
 
-    # Convert cuda to numpy for openCV frame format
-    roi = jetson_utils.cudaToNumpy(roi,dispW,dispH,4)
-    roi = cv2.cvtColor(roi,cv2.COLOR_RGBA2BGR).astype(np.uint8)
+        # Draw rectangle on frame
+        frame=cv2.rectangle(frame,(300,0),(500,dispH),(160,160,160),2)
 
-    
-    # Format text for openCV
-    cv2.putText(frame,str(round(confident*100, ndigits=1)) + ' confidence ' + str(round(fpsFilter,1))+' fps '+  item,(0,30),font,1,(0,200,255),2,True)
+        # Format text for openCV
+        cv2.putText(frame,str(round(confident*100, ndigits=1)) + ' confidence ' + str(round(fpsFilter,1))+' fps '+  item,(0,30),font,1,(0,200,255),2)
 
-    roi = frame[0:1024,412:612].copy()
-    # Display image as camera
-    cv2.imshow('Camera',frame)
-    cv2.imshow('ROI', roi)
-    # Move display to 0,0 area
-    # cv2.moveWindow('Camera',0,0)
+        # Crop frame for ROI
+        roi = frame[0:600,300:500].copy()
 
-    # Loop to exit from camera
-    if cv2.waitKey(1) == ord('q'):
-        break
-
+        # Display image as camera
+        cv2.imshow('AOI',frame)
+        cv2.imshow('ROI', roi)
+        # Move display to 0,0 area
+        cv2.moveWindow('ROI',350,0)
         
-# Release and destroy windows
-cam.release()
+        if cv2.waitKey(1) == ord('r'):
+            preview = False
+            aoi = False
+            roi = True
+            cam.Close()
+            cv2.destroyAllWindows()
+        elif cv2.waitKey(1) == ord('a'):
+            preview = False
+            aoi = True
+            roi = False
+            cam.Close()
+            cv2.destroyAllWindows()
+
+    if wide_aoi:
+         # Capture frame, specify width and height
+        frame, width, height = cam.CaptureRGBA(zeroCopy=1)
+
+        # Network Classifier to return ID and confidence level
+        classID, confident = net.Classify(frame, width, height)
+        # Changes classID number into item name
+        item = net.GetClassDesc(classID)
+
+
+        # Write item to pico over UART
+        # try:
+        #     if item != 'mail':
+        print(item)
+        serial_port.write(item.encode())
+
+        # except:
+        #     print('Continuing')
+
+        # Motor control ?
+        
+
+        # Calculate time difference for fps
+        dt = time.time()-timeMark
+        # fps calculation
+        fps = 1/dt
+        # Smoothing calculation using Filter
+        fpsFilter = .95*fpsFilter + .05*fps
+        # timeMark for Filter
+        timeMark = time.time()
+        # Convert cuda to numqpy for openCV frame format
+        frame = jetson_utils.cudaToNumpy(frame,width,height,4)
+        frame = cv2.cvtColor(frame,cv2.COLOR_RGBA2BGR).astype(np.uint8)
+
+        # Format text for openCV
+        cv2.putText(frame,str(round(confident*100, ndigits=1)) + ' confidence ' + str(round(fpsFilter,1))+' fps '+  item,(0,30),font,1,(0,200,255),2)
+
+        # Display image as camera
+        cv2.imshow('Camera',frame)
+        # Move display to 0,0 area
+        cv2.moveWindow('Camera',0,0)
+        if cv2.waitKey(1) == ord('r'):
+            preview = False
+            aoi = False
+            aoiroi = True
+            cam.Close()
+            cv2.destroyAllWindows()
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+# Release and destroy windows  
+cam.Close()
 cv2.destroyAllWindows()
